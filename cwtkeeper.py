a0bef436494eb18b3d1ffa743b49e1c7653f22e7
tanupoo <github-shoichi@tanu.org>
2021-11-09 12:04:14 +0900

    added a function to delete users and revised to specify a list of email addresses.

--- cwtkeeper.py
+++ cwtkeeper.py
@@ -3,6 +3,7 @@
 from webexteamssdk import WebexTeamsAPI, exceptions
 from typing import List, Union
 import os
+import json
 
 """
 https://github.com/CiscoDevNet/webexteamssdk
@@ -25,6 +26,18 @@ def member_add(api: WebexTeamsAPI, room_id: str, mail_addr: list) -> None:
         else:
             print(f"{m} has been added.")
 
+def member_delete(api: WebexTeamsAPI, room_id: str, mail_addr: list) -> None:
+    for m in mail_addr:
+        try:
+           n = api.memberships.list(room_id, personEmail=m)
+           ret = api.memberships.delete(list(n)[0].id)
+        except IndexError:
+            print(f"{m} is not a member.")
+        except exceptions.ApiError as e:
+            print(str(e))
+        else:
+            print(f"{m} has been deleted.")
+
 def send_msg(
         api: WebexTeamsAPI,
         room_id: str,
@@ -46,9 +59,14 @@ def send_msg(
                                 files=file_list)
     print(ret)
 
-def member_list(api: WebexTeamsAPI, room_id: str) -> None:
-    for m in api.memberships.list(room_id):
-        print(f"{m.personEmail}: {m.personDisplayName}")
+def member_show(api: WebexTeamsAPI, room_id: str, mail_addr: list) -> None:
+    if mail_addr:
+        for m in mail_addr:
+            for n in api.memberships.list(room_id, personEmail=m):
+                print(f"MembershipId: {n.id}")
+    else:
+        for n in api.memberships.list(room_id):
+            print(f"{n.personDisplayName}: {n.personEmail}: {n.id}")
 
 def main():
     from argparse import ArgumentParser
@@ -65,20 +83,32 @@ def main():
                     help="specify the room id.")
     ap.add_argument("--show-members", "-M", action="store_true",
                     dest="show_members",
-                    help="specify to show the members in the room.")
-    ap.add_argument("--add-members", "-a", action="store", dest="member_emails",
-                    help="specify a list of the member's email address "
+                    help="inform to show the members in the room. "
+                    "with the -e option, show the info of the specific members")
+    ap.add_argument("--add-members", "-A", action="store_true",
+                    dest="add_members",
+                    help="with the -e option, inform to add members")
+    ap.add_argument("--delete-members", "-D", action="store_true",
+                    dest="delete_members",
+                    help="with the -e option, inform to delete members")
+    ap.add_argument("--email-addrs", "-e", action="store", dest="email_addrs",
+                    help="specify the member's email addresses "
                     "separated by a comma.")
     ap.add_argument("-m", action="store", dest="message",
                     help="specify a message to be sent.")
     ap.add_argument("-f", action="append", dest="files", nargs="*",
                     help="specify a file to be sent. "
                         "specify multiple times to send multiple files. ")
+    ap.add_argument("--config", "-c", action="store", dest="config",
+                    help="NOTYET: specify the config file.")
+    ap.add_argument("--update-config", action="store_true", dest="update_config",
+                    help="NOTYET: specify to create or update the config file.")
     ap.add_argument("-v", action="store_true", dest="verbose",
                     help="enable verbose mode.")
-    ap.add_argument("-d", action="store_true", dest="debug",
-                    help="enable debug mode.")
     opt = ap.parse_args()
+
+    if opt.config:
+        config = json.load(open(opt.config))
 
     # set the access token
     # the one specifyed by the option is preferred than the env variable.
@@ -87,6 +117,13 @@ def main():
         if opt.access_token is None:
             print("ERROR: access_token must be specified.")
             exit(0)
+
+    email_addrs_list = None
+    if opt.email_addrs:
+        email_addrs_list = [i for i in opt.email_addrs.replace(" ","").split(",")]
+    elif (opt.add_members or opt.delete_members):
+        ap.print_help()
+        exit(0)
 
     # just try to set the room id if available.
     if opt.room_id is None:
@@ -98,12 +135,11 @@ def main():
         exit(0)
     if opt.room_id:
         if opt.show_members:
-            member_list(api, opt.room_id)
-        elif opt.member_emails:
-            member_add(
-                    api,
-                    opt.room_id,
-                    [i for i in opt.member_emails.replace(" ","").split(",")])
+            member_show(api, opt.room_id, email_addrs_list)
+        elif opt.add_members:
+            member_add(api, opt.room_id, email_addrs_list)
+        elif opt.delete_members:
+            member_delete(api, opt.room_id, email_addrs_list)
         elif opt.message:
             send_msg(
                     api,
