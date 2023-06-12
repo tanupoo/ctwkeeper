3ed2ca23c0a071afc7b25050d732659a11bb06e1
tanupoo <github-shoichi@tanu.org>
2023-06-12 17:40:42 +0900

    modified and modularized the functions. now, you have to specify the function such as room.

--- cwtkeeper.py
+++ cwtkeeper.py
@@ -1,132 +1,267 @@
 #!/usr/bin/env python
 
-from webexteamssdk import WebexTeamsAPI, exceptions
-from typing import List, Union
+from webexteamssdk import WebexTeamsAPI, exceptions, WebexTeamsDateTime
 import os
 import json
+from argparse import ArgumentParser
 
 """
 https://github.com/CiscoDevNet/webexteamssdk
 """
 
-# Find all rooms that have 'webexteamssdk Demo' in their title
-def room_list(api: WebexTeamsAPI) -> None:
-    for r in api.rooms.list():
-        print(f"{r.title}: {r.id}")
+#
+# room management
+#
+def room_functions(opt, subp):
+    # set the room id if not specified.
+    if opt.room_id is None:
+        opt.room_id = os.environ.get("WEBEX_TEAMS_ROOM_ID", None)
 
-def member_add(api: WebexTeamsAPI, room_id: str, mail_addr: list) -> None:
-    for m in mail_addr:
-        try:
-            ret = api.memberships.create(room_id, personEmail=m)
-        except exceptions.ApiError as e:
-            if "User is already a participant" in str(e):
-                print(f"{m} is already participated.")
+    api = WebexTeamsAPI(access_token=opt.access_token)
+
+    if opt.show_room_list:
+        # Find all rooms where the bot participates.
+        for r in api.rooms.list():
+            print(f"{r.title}: {r.id}")
+    elif opt.room_id:
+        ret = api.rooms.get(opt.room_id)
+        print(ret)
+
+
+#
+# membership management
+#
+def member_functions(opt, subp):
+    # check room_id
+    if opt.room_id is None:
+        opt.room_id = os.environ.get("WEBEX_TEAMS_ROOM_ID", None)
+    if opt.room_id is None:
+        subp.choices[opt.subcommand].print_help()
+        return
+    # set the mail addrs.
+    opt.mail_addrs = []
+    if opt._mail_addrs:
+        opt.mail_addrs = [i for i in opt._mail_addrs.replace(" ","").split(",")]
+
+    api = WebexTeamsAPI(access_token=opt.access_token)
+
+    if opt.show_members:
+        if opt.mail_addrs:
+            for a in opt.mail_addrs:
+                for n in api.memberships.list(opt.room_id, personEmail=a):
+                    print(f"MembershipId: {n.id}")
+        else:
+            for n in api.memberships.list(opt.room_id):
+                print(f"{n.personDisplayName}: {n.personEmail}: {n.id}")
+
+    elif opt.add_members:
+        for a in opt.mail_addr:
+            try:
+                ret = api.memberships.create(opt.room_id, personEmail=a)
+            except exceptions.ApiError as e:
+                if "User is already a participant" in str(e):
+                    print(f"{a} is already participated.")
+                else:
+                    print(str(e))
             else:
+                print(f"{a} has been added.")
+
+    elif opt.delete_members:
+        for a in opt.mail_addr:
+            try:
+                n = api.memberships.list(opt.room_id, personEmail=a)
+                ret = api.memberships.delete(list(n)[0].id)
+            except IndexError:
+                print(f"{a} is not a member.")
+            except exceptions.ApiError as e:
                 print(str(e))
+            else:
+                print(f"{a} has been deleted.")
+
+#
+# message functions
+#
+def message_functions(opt, subp):
+    # set the room id if not specified.
+    if opt.room_id is None:
+        opt.room_id = os.environ.get("WEBEX_TEAMS_ROOM_ID", None)
+    # set the mail addrs.
+    opt.mail_addrs = []
+    if opt._mail_addrs:
+        opt.mail_addrs = [i for i in opt._mail_addrs.replace(" ","").split(",")]
+
+    api = WebexTeamsAPI(access_token=opt.access_token)
+
+    if opt.show_messages:
+        if not (opt.max_nb_mails or opt._since):
+            subp.choices[opt.subcommand].print_help()
+            return
+        # set datetime of opt.since
+        since_dt = None
+        if opt.since:
+            since_dt = WebexTeamsDateTime.strptime(opt.since)
+        ret = None
+        if opt.mail_addrs:
+            for a in opt.mail_addrs:
+                ret = api.messages.list(roomId=opt.room_id,
+                                        mentionedPeople=a)
+                for i,m in enumerate(ret):
+                    if since_dt:
+                        if since_dt > m.created:
+                            break
+                    print(m)
+                    if i + 1 >= opt.max_nb_mails:
+                        break
         else:
-            print(f"{m} has been added.")
-
-def member_delete(api: WebexTeamsAPI, room_id: str, mail_addr: list) -> None:
-    for m in mail_addr:
-        try:
-           n = api.memberships.list(room_id, personEmail=m)
-           ret = api.memberships.delete(list(n)[0].id)
-        except IndexError:
-            print(f"{m} is not a member.")
-        except exceptions.ApiError as e:
-            print(str(e))
-        else:
-            print(f"{m} has been deleted.")
-
-def send_msg(
-        api: WebexTeamsAPI,
-        room_id: str,
-        message: Union[str,None] = None,
-        files: Union[List,None] = None
-        ) -> None:
-    if files is not None and len(files) > 1:
-        raise NotImplementedError(
-                "ERROR: don't specify multiple files. "
-                "It currently doesn't support the feature "
-                "due to the SDK's constraints.")
-    file_list = None
-    if files is not None:
-        file_list = []
-        for f in files[0]:
-            file_list.append(os.path.abspath(f))
-    ret = api.messages.create(room_id,
-                                text=message,
-                                files=file_list)
-    print(ret)
-
-def member_show(api: WebexTeamsAPI, room_id: str, mail_addr: list) -> None:
-    if mail_addr:
-        for m in mail_addr:
-            for n in api.memberships.list(room_id, personEmail=m):
-                print(f"MembershipId: {n.id}")
+            ret = api.messages.list(roomId=opt.room_id)
+            for i,m in enumerate(ret):
+                if since_dt:
+                    if since_dt > m.created:
+                        break
+                print(m)
+                if i + 1 >= opt.max_nb_mails:
+                    break
     else:
-        for n in api.memberships.list(room_id):
-            print(f"{n.personDisplayName}: {n.personEmail}: {n.id}")
+        file_list = []
+        if opt._files:
+            for f in files[0]:
+                file_list.append(os.path.abspath(f))
+        if len(file_list) not in [0, 1]:
+            raise NotImplementedError(
+                    "ERROR: don't specify multiple files. "
+                    "It currently doesn't support the feature "
+                    "due to the SDK's constraints.")
 
-def people_show(api: WebexTeamsAPI, mail_addr: str) -> None:
-    for m in mail_addr:
-        try:
-            ret = api.people.list(email=m)
-        except exceptions.ApiError as e:
-            if "User is already a participant" in str(e):
-                print(f"{m} is already participated.")
-            else:
-                print(str(e))
+        if not (opt.message or file_list):
+            subp.choices[opt.subcommand].print_help()
+            return
+
+        elif opt.mail_addrs:
+            for a in opt.mail_addrs:
+                ret = api.messages.create(toPersonEmail=a,
+                                          text=opt.message,
+                                          files=file_list)
+            print(ret)
+        elif opt.room_id:
+            ret = api.messages.create(roomId=opt.room_id,
+                                      text=opt.message,
+                                      files=file_list)
+            print(ret)
         else:
-            for i in ret:
-                print(i)
+            subp.choices[opt.subcommand].print_help()
+            return
 
-def main():
-    from argparse import ArgumentParser
-    from argparse import ArgumentDefaultsHelpFormatter
-    ap = ArgumentParser(
-            description="Webex Teams Keeper.",
-            formatter_class=ArgumentDefaultsHelpFormatter)
-    ap.add_argument("--token", "-t", action="store", dest="access_token",
-                    help="specify the access token.")
-    ap.add_argument("--show-rooms", "-L", action="store_true",
-                    dest="show_room_list",
-                    help="specify to show the list of rooms.")
-    ap.add_argument("--room-id", "-r", action="store", dest="room_id",
-                    help="specify the room id.")
-    ap.add_argument("--show-members", "-M", action="store_true",
-                    dest="show_members",
-                    help="inform to show the members in the room. "
-                    "with the -e option, show the info of the specific members")
-    ap.add_argument("--add-members", "-A", action="store_true",
-                    dest="add_members",
-                    help="with the -e option, inform to add members")
-    ap.add_argument("--delete-members", "-D", action="store_true",
-                    dest="delete_members",
-                    help="with the --email-addrs option, "
-                        "inform to delete members")
-    ap.add_argument("--email-addrs", "-e", action="store", dest="email_addrs",
-                    help="specify the member's email addresses "
-                    "separated by a comma.")
-    ap.add_argument("-m", action="store", dest="message",
-                    help="specify a message to be sent.")
-    ap.add_argument("-f", action="append", dest="files", nargs="*",
-                    help="specify a file to be sent. "
-                        "specify multiple times to send multiple files. ")
-    ap.add_argument("--show-people", action="store_true", dest="show_people",
-                    help="specify to show information of the certain people. "
-                    "the --email-addrs option is required.")
+#
+# people functions
+#
+def people_functions(opt, subp):
+    # set the mail addrs.
+    opt.mail_addrs = []
+    if opt._mail_addrs:
+        opt.mail_addrs = [i for i in opt._mail_addrs.replace(" ","").split(",")]
+
+    api = WebexTeamsAPI(access_token=opt.access_token)
+
+    for a in opt.mail_addrs:
+        for p in api.people.list(email=a):
+            if opt.show_detail:
+                print(p)
+            else:
+                print(f"{p.displayName}: {p.id}")
+
+def main(argv):
+    ap = ArgumentParser(description="Webex Teams Keeper.")
+    """
     ap.add_argument("--config", "-c", action="store", dest="config",
                     help="NOTYET: specify the config file.")
     ap.add_argument("--update-config", action="store_true", dest="update_config",
                     help="NOTYET: specify to create or update the config file.")
-    ap.add_argument("-v", action="store_true", dest="verbose",
-                    help="enable verbose mode.")
-    opt = ap.parse_args()
-
+    """
+    # sub menu
+    subp = ap.add_subparsers(title="subcommands",
+                             metavar="room, member, message, people",
+                             dest="subcommand",
+                             required=True,
+                             )
+    # room
+    sap = subp.add_parser("room", aliases=["roo", "rm"],
+                          help="the room management.")
+    sap.add_argument("--token", "-t", action="store", dest="access_token",
+                     help="specify the access token.")
+    sap.add_argument("--room-id", "-r", action="store", dest="room_id",
+                     help="specify the room id.")
+    sap.add_argument("--list", "-l", action="store_true",
+                     dest="show_room_list",
+                     help="show the list of rooms.")
+    sap.set_defaults(func=room_functions)
+    # membership
+    sap = subp.add_parser("member", aliases=["mem", "mm"],
+                          help="the membership management.")
+    sap.add_argument("--token", "-t", action="store", dest="access_token",
+                    help="specify the access token.")
+    sap.add_argument("--room-id", "-r", action="store", dest="room_id",
+                     required=True,
+                    help="specify the room id.")
+    sap.add_argument("--list", "-l", action="store_true", dest="show_members",
+                     help="inform to show the members in the room. "
+                     "with the --email option, show the info of "
+                     "the specific members")
+    sap.add_argument("--add", "-a", action="store_true", dest="add_members",
+                    help="with the --email-addrs option, inform to add members")
+    sap.add_argument("--delete", "-d", action="store_true",
+                    dest="delete_members",
+                    help="with the --email-addrs option, "
+                        "inform to delete members")
+    sap.add_argument("--email", "-e", action="store", dest="_mail_addrs",
+                    help="specify the member's email addresses "
+                    "separated by a comma.")
+    sap.set_defaults(func=member_functions)
+    # message
+    sap = subp.add_parser("message", aliases=["mes", "msg", "mesg", "ms"],
+                          help="the message management.")
+    sap.add_argument("--token", "-t", action="store", dest="access_token",
+                     help="specify the access token.")
+    sap.add_argument("--room-id", "-r", action="store", dest="room_id",
+                     help="specify the room id. "
+                     "Ignored with the --email option.")
+    sap.add_argument("--email", "-e", action="store", dest="_mail_addrs",
+                     help="specify the recipient's email addresses "
+                    "separated by a comma.")
+    sap.add_argument("--message", "-m", action="store", dest="message",
+                     help="specify a message to be sent.")
+    sap.add_argument("--file", "-f", action="append", dest="_files", nargs="*",
+                     help="specify a file to be sent. "
+                        "specify multiple times to send multiple files. ")
+    sap.add_argument("--list", "-l", action="store_true", dest="show_messages",
+                     help="list the messages.  "
+                     "Either the --since option or the --max option is required.")
+    sap.add_argument("--since", "-a", action="store", dest="since",
+                     help="specify ISO6801 datetime to be listed mails "
+                     "after the datetime.")
+    sap.add_argument("--max", action="store", dest="max_nb_mails",
+                     type=int, default=16,
+                     help="specify the number of mails to be listed.")
+    sap.set_defaults(func=message_functions)
+    # people
+    sap = subp.add_parser("people", aliases=["peo", "ppl"],
+                          help="the people management. "
+                          "ONLY FOR NON-ADMIN USERS.")
+    sap.add_argument("--token", "-t", action="store", dest="access_token",
+                    help="specify the access token.")
+    sap.add_argument("--email-addrs", "-e", action="store", dest="_mail_addrs",
+                     metavar="MAIL_ADDRS",
+                    help="specify the member's email addresses "
+                    "separated by a comma. "
+                    "it is required if you are not admin.")
+    sap.add_argument("--detail", "-L", action="store_true", dest="show_detail",
+                     help="specify to show a detail info.")
+    sap.set_defaults(func=people_functions)
+    #
+    opt = ap.parse_args(argv)
+    """
     if opt.config:
         config = json.load(open(opt.config))
-
+    """
     # set the access token
     # the one specifyed by the option is preferred than the env variable.
     if opt.access_token is None:
@@ -134,42 +269,13 @@ def main():
         if opt.access_token is None:
             print("ERROR: access_token must be specified.")
             exit(0)
-
-    email_addrs_list = None
-    if opt.email_addrs:
-        email_addrs_list = [i for i in opt.email_addrs.replace(" ","").split(",")]
-    elif (opt.add_members or opt.delete_members):
-        ap.print_help()
-        exit(0)
-
-    # just try to set the room id if available.
-    if opt.room_id is None:
-        opt.room_id = os.environ.get("WEBEX_TEAMS_ROOM_ID", None)
-
-    api = WebexTeamsAPI(access_token=opt.access_token)
-    # peple
-    if opt.show_people:
-        people_show(api, email_addrs_list)
-    elif opt.show_room_list:
-    # room
-        room_list(api)
-    elif opt.room_id:
-        if opt.show_members:
-            member_show(api, opt.room_id, email_addrs_list)
-        elif opt.add_members:
-            member_add(api, opt.room_id, email_addrs_list)
-        elif opt.delete_members:
-            member_delete(api, opt.room_id, email_addrs_list)
-        elif opt.message:
-            send_msg(
-                    api,
-                    opt.room_id,
-                    message=opt.message,
-                    files=opt.files)
+    # call a function.
+    opt.func(opt, subp)
 
 #
 # main
 #
 if __name__ == "__main__" :
-    main()
+    import sys
+    main(sys.argv[1:])
 
